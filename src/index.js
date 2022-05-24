b6c7e5d4376affa72a5b8c577149319b84bea6d7
“jinzhe” <jinzhe961202@163.com>
2022-05-24 15:08:07 +0800

    master

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * @作者: kerwin
+ * @公众号: 大前端私房菜
+ */
+// console.log("hello world kerwin")
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from './01-base/12-卖座选项卡'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// import {Provider}  from 'mobx-react'
+// import store from './10-mobx/04-router/mobx/store'
+ReactDOM.render(
+    // <React.StrictMode>
+    // <Provider store={store}>
+        <App/>
+    // </Provider>
+    
+    // </React.StrictMode>
+    ,document.getElementById("root"))
+
